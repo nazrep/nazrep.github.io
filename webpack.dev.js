af51d38827ae281a655c0e6f4c22d29ae52baa63
Maciej Perzankowski <perzankowski.m2001@gmail.com>
2020-07-07 14:09:28 +0200

    feat: hero & skills markup

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,46 +2,45 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  devtool: "source-map",
-  mode: "development",
+    devtool: "source-map",
+    mode: "development",
 
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        loader: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
+    module: {
+        rules: [{
+                test: /\.s[ac]ss$/i,
+                loader: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
             },
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              sourceMap: true,
+            {
+                test: /\.css$/i,
+                loader: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
             },
-          },
         ],
-      },
-      {
-        test: /\.css$/i,
-        loader: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              sourceMap: true,
-            },
-          },
-        ],
-      },
-    ],
-  },
+    },
 
-  devServer: {
-    port: 8080,
-    host: '0.0.0.0'
-  },
+    devServer: {
+        port: 8080,
+        host: '0.0.0.0'
+    },
 });
